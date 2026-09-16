95eef2186f7bb361015da973839a9a7799d2f57c
marcinhubicki-coder <marcinhubicki@gmail.com>
2026-09-16 15:28:34 +0200

    Add English vocabulary body catalog

---
+++ data/english/body.mjs
@@ -0,0 +1,33 @@
+// word|Polish meaning|category|difficulty 1-3|wrong answers (semicolon-separated)
+export default String.raw`
+head|głowa|body|1|haed;ehad;hed
+face|twarz|body|1|fcae;afce;fae
+hair|włosy|body|1|hiar;ahir;har
+eye|oko|body|1|eey;yee;eyye
+ear|ucho|body|1|era;aer;eaar
+nose|nos|body|1|nsoe;onse;noe
+mouth|usta|body|2|motuh;omuth;moth
+tooth|ząb|body|2|totoh;ototh;toth
+teeth|zęby|body|2|teteh;eteth;teth
+neck|szyja|body|1|ncek;enck;nek
+shoulder|ramię|body|3|sholuder;hsoulder;shouder
+arm|ręka|body|1|amr;ram;arrm
+hand|dłoń|body|1|hnad;ahnd;had
+finger|palec|body|2|figner;ifnger;finer
+thumb|kciuk|body|2|thmub;htumb;thmb
+chest|klatka piersiowa|body|2|chset;hcest;chst
+back|plecy|body|1|bcak;abck;bak
+stomach|brzuch|body|2|stoamch;tsomach;stoach
+leg|noga|body|1|lge;elg;leeg
+knee|kolano|body|2|kene;nkee;kne
+foot|stopa|body|1|foto;ofot;fot
+feet|stopy|body|1|fete;efet;fet
+toe|palec u nogi|body|1|teo;ote;tooe
+heart|serce|body|2|herat;ehart;hert
+brain|mózg|body|2|brian;rbain;brin
+skin|skóra|body|1|sikn;ksin;skn
+blood|krew|body|2|bolod;lbood;blod
+bone|kość|body|1|bnoe;obne;boe
+body|ciało|body|1|bdoy;obdy;boddy
+voice|głos|body|2|vocie;ovice;voce
+`;

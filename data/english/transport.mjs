084f853e2167ee9b8000f721f7e9e50320e7a80c
marcinhubicki-coder <marcinhubicki@gmail.com>
2026-09-16 15:30:33 +0200

    Add English vocabulary transport catalog

---
+++ data/english/transport.mjs
@@ -0,0 +1,28 @@
+// word|Polish meaning|category|difficulty 1-3|wrong answers (semicolon-separated)
+export default String.raw`
+car|samochód|transport|1|cra;acr;caar
+bus|autobus|transport|1|bsu;ubs;buus
+train|pociąg|transport|2|trian;rtain;trin
+bicycle|rower|transport|2|bisycle;bycicle;bicicle
+bike|rower|transport|1|bkie;ibke;bie
+plane|samolot|transport|2|plnae;lpane;plne
+airplane|samolot|transport|3|airlpane;iarplane;airpane
+taxi|taksówka|transport|1|txai;atxi;tai
+truck|ciężarówka|transport|2|trcuk;rtuck;trck
+van|furgonetka|transport|1|vna;avn;vaan
+motorcycle|motocykl|transport|3|motocrycle;omtorcycle;motorycle
+boat|łódź|transport|1|baot;obat;bot
+ship|statek|transport|1|sihp;hsip;shp
+tram|tramwaj|transport|1|tarm;rtam;trm
+subway|metro|transport|2|suwbay;usbway;subay
+ticket|bilet|transport|2|tikcet;itcket;ticet
+journey|podróż|transport|2|jounrey;ojurney;jouney
+trip|wycieczka|transport|1|tirp;rtip;trp
+travel|podróżowanie|transport|2|trvael;rtavel;trael
+traffic|ruch uliczny|transport|2|trfafic;rtaffic;trafic
+passenger|pasażer|transport|3|passneger;apssenger;passnger
+wheel|koło|transport|3|wehel;hweel;whel
+engine|silnik|transport|2|enigne;negine;engne
+route|trasa|transport|2|rotue;orute;rote
+platform|peron|transport|3|plaftorm;lpatform;platorm
+`;

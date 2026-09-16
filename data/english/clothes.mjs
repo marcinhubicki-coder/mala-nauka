c62b70c7d08498a92ef5a9f49e2882b2508d0cf1
marcinhubicki-coder <marcinhubicki@gmail.com>
2026-09-16 15:30:46 +0200

    Add English vocabulary clothes catalog

---
+++ data/english/clothes.mjs
@@ -0,0 +1,23 @@
+// word|Polish meaning|category|difficulty 1-3|wrong answers (semicolon-separated)
+export default String.raw`
+clothes|ubrania|clothes|2|clohtes;lcothes;clohes
+shirt|koszula|clothes|2|shrit;hsirt;shrt
+t-shirt|koszulka|clothes|3|tshirt;t shirt;t-shrit
+trousers|spodnie|clothes|3|trosuers;rtousers;trouers
+pants|spodnie|clothes|2|patns;apnts;pats
+dress|sukienka|clothes|2|derss;rdess;drss
+skirt|spódnica|clothes|2|skrit;ksirt;skrt
+jacket|kurtka|clothes|2|jakcet;ajcket;jacet
+coat|płaszcz|clothes|1|caot;ocat;cot
+sweater|sweter|clothes|2|swetaer;wseater;sweter
+shoe|but|clothes|1|sohe;hsoe;she
+boot|but zimowy|clothes|1|boto;obot;bot
+sock|skarpetka|clothes|1|scok;osck;sok
+hat|kapelusz|clothes|1|hta;aht;haat
+cap|czapka z daszkiem|clothes|1|cpa;acp;caap
+glove|rękawiczka|clothes|2|glvoe;lgove;glve
+scarf|szalik|clothes|3|scraf;csarf;scrf
+belt|pasek|clothes|1|blet;eblt;bet
+pocket|kieszeń|clothes|2|pokcet;opcket;pocet
+button|guzik|clothes|2|butotn;ubtton;buton
+`;

4a803566f4e1bdbeca45e07e162df2d17adc7ff1
marcinhubicki-coder <marcinhubicki@gmail.com>
2026-09-16 15:28:25 +0200

    Add English vocabulary people catalog

---
+++ data/english/people.mjs
@@ -0,0 +1,23 @@
+// word|Polish meaning|category|difficulty 1-3|wrong answers (semicolon-separated)
+export default String.raw`
+man|mężczyzna|people|1|mna;amn;maan
+woman|kobieta|people|2|woamn;owman;woan
+boy|chłopiec|people|1|byo;oby;booy
+girl|dziewczynka|people|1|gril;igrl;gil
+person|osoba|people|2|pesron;eprson;peron
+people|ludzie|people|2|pepole;epople;peole
+friend|przyjaciel|people|3|freind;rfiend;frind
+neighbour|sąsiad|people|3|neigbhour;enighbour;neigbour
+guest|gość|people|2|guset;ugest;gust
+adult|dorosły|people|2|adlut;dault;adlt
+teenager|nastolatek|people|3|teengaer;etenager;teenger
+stranger|nieznajomy|people|3|strnager;tsranger;strager
+partner|partner|people|2|parnter;aprtner;parner
+couple|para|people|2|copule;ocuple;coule
+group|grupa|people|2|gruop;rgoup;grup
+team|zespół|people|1|taem;etam;tem
+crowd|tłum|people|2|crwod;rcowd;crwd
+classmate|kolega z klasy|people|3|clasmsate;lcassmate;clasmate
+colleague|kolega z pracy|people|3|collaegue;oclleague;collague
+boss|szef|people|1|bsos;obss;bos
+`;

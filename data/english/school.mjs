6ea4dfd42025175245b6ffb53699b38a0222d5e2
marcinhubicki-coder <marcinhubicki@gmail.com>
2026-09-16 15:29:14 +0200

    Add English vocabulary school catalog

---
+++ data/english/school.mjs
@@ -0,0 +1,33 @@
+// word|Polish meaning|category|difficulty 1-3|wrong answers (semicolon-separated)
+export default String.raw`
+school|szkoła|school|3|scohol;cshool;schol
+classroom|klasa|school|3|clasrsoom;lcassroom;clasroom
+lesson|lekcja|school|2|lesosn;elsson;leson
+subject|przedmiot|school|2|subejct;usbject;subect
+homework|praca domowa|school|3|homweork;ohmework;homeork
+test|test|school|1|tset;etst;tet
+exam|egzamin|school|1|eaxm;xeam;exm
+question|pytanie|school|3|quetsion;uqestion;quesion
+answer|odpowiedź|school|2|anwser;naswer;anser
+exercise|ćwiczenie|school|3|execrise;xeercise;exerise
+task|zadanie|school|1|tsak;atsk;tak
+word|słowo|school|1|wrod;owrd;wod
+sentence|zdanie|school|3|senetnce;esntence;sentnce
+story|opowiadanie|school|2|stroy;tsory;stry
+number|liczba|school|2|nubmer;unmber;numer
+page|strona|school|1|pgae;apge;pae
+paper|papier|school|2|paepr;apper;paer
+ruler|linijka|school|2|ruelr;urler;ruer
+eraser|gumka|school|2|ersaer;reaser;eraer
+board|tablica|school|2|borad;obard;bord
+desk|biurko|school|1|dsek;edsk;dek
+break|przerwa|school|2|braek;rbeak;brak
+grade|ocena|school|2|grdae;rgade;grde
+mark|ocena|school|1|mrak;amrk;mak
+language|język|school|3|lanugage;alnguage;langage
+dictionary|słownik|school|3|dictoinary;idctionary;dictinary
+maths|matematyka|school|2|mahts;amths;mahs
+history|historia|school|2|hisotry;ihstory;hisory
+science|nauka|school|3|scinece;csience;scince
+music|muzyka|school|2|muisc;umsic;muic
+`;
